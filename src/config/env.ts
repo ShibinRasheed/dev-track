e2c862c806e0605f2bc8783f44dc67f79ac50589
Shibin Rasheed <shibinrasheed333@gmail.com>
2026-07-20 22:26:25 +0530

    chore : add env config

---
+++ src/config/env.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["development", "production"]).default("development"),
+  PORT: z.coerce.number(),
+});
+
+export const env = envSchema.parse(process.env);
